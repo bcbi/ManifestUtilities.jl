7fdf2abab13fb16d782a85bd403fbc2db5391d4d
Documenter.jl <documenter@juliadocs.github.io>
2021-05-27 23:51:10 +0000

    build based on d9a928d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ManifestStuff","category":"page"},{"location":"#ManifestStuff","page":"Home","title":"ManifestStuff","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ManifestStuff.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ManifestStuff]\nPublic = true\nPrivate = false","category":"page"}]
+}
